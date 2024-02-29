ecf34c116cd04630f5929bd64b2b78637c6e327f
bianca.andrade <bc.andrade05@gmail.com>
2024-02-28 21:18:47 -0300

    insere codigo do app - terceiro dia

--- database.js
+++ database.js
@@ -1,1 +1,17 @@
 // Vamos incluir a configuração do banco de dados aqui
+
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const connectDatabase = () => {
+  console.log("Conectando com MongoDB");
+
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB conectado com sucesso!"))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDatabase;
